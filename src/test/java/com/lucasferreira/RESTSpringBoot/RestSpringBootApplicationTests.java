0d8f160be34fd42ea3bba577f5729e8f09b4ccf3
lcseferreira <lcseferreira@hotmail.com>
2021-09-02 16:56:25 -0300

    First commit and configs

---
+++ src/test/java/com/lucasferreira/RESTSpringBoot/RestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasferreira.RESTSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
